6a2f69442301e6cb73922e1bab43175446445e65
Устинов Тихон <tih.ustinov2013@yandex.ru>
2019-07-15 19:15:12 +0900

    Update settings.js

--- module/settings.js
+++ module/settings.js
@@ -13,8 +13,8 @@ const options = {
 
 
 module.exports = {
-    GROUP_TOKEN: "1e374f25bdf2ce4b98c4674c2c60979cee9ecb1953962f38abcd0e9e706d0872099ba433bc33900c7fa4d",
-    ADMIN_TOKEN: "906ccfe8dd7338e654dd97a178b703d9524d4f1155d0c87ac99968b037b7cbf2c42d4f59bed786a9be516",
+    GROUP_TOKEN: "goup_token",
+    ADMIN_TOKEN: "you_token",
     SECRET: "FuckSecret",
     ID_GROUP: 144267450,
     db_pass:PASSDB,
